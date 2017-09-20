3bec71ad9bbc24d57ad21c6604fd257a5321f0e3
WolakT <wolak.tom@gmail.com>
2017-09-20 19:38:15 +0200

    upd

--- ads_database script.sql
+++ ads_database script.sql
@@ -3,13 +3,13 @@ create database ads_service;
 use ads_service;
 
 CREATE TABLE users (
-  username VARCHAR(45) NOT NULL ,
-  password VARCHAR(45) NOT NULL ,
-  enabled TINYINT NOT NULL DEFAULT 1 ,
-  phone_No int not null,
-  mail text not null,
-  PRIMARY KEY (username));
-  
+ username VARCHAR(45) NOT NULL ,
+ password VARCHAR(45) NOT NULL ,
+ enabled TINYINT NOT NULL DEFAULT 1 ,
+ phone_No varchar(45) not null,
+ mail text not null,
+ city varchar(45) not null,
+ PRIMARY KEY (username)); 
  
 
 Create Table Ads (
@@ -20,5 +20,11 @@ Create Table Ads (
   );
   
   Alter table Ads add foreign key (username) references users(username) on delete restrict;
+
+CREATE TABLE user_roles (
+ user_role_id int(11) NOT NULL AUTO_INCREMENT,
+ username varchar(45) NOT NULL,
+ role varchar(45) NOT NULL,
+ PRIMARY KEY (user_role_id));
 
 alter table users add column city varchar(44) not null;
